532cc471bbb0575a7b85f242cdf444c73b1e6dde
Erika Williams <erika@erikawilliams.us>
2022-03-21 08:53:02 -0600

    commit tree 1a188ad384d6c691f49e11a2a595b7bcc0923447

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = '100px';
+
+const leftNumbers = dodger.style.left.replace('px','');
+const left = parseInt(leftNumbers,10);
+
+function moveDodgerLeft(){
+    if (left>0){
+        dodger.style.left = `${left-3}px`;
+    }
+}
+
+document.addEventListener('keydown',function(event){
+    if(event.key === 'ArrowLeft'){
+        moveDodgerLeft()
+    }
+})
+
+function moveDodgerRight(){
+    if(left < 400){
+        dodger.style.left = `${left+1}px`
+    }
+}
+
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowRight'){
+        moveDodgerRight()
+    }
+})
